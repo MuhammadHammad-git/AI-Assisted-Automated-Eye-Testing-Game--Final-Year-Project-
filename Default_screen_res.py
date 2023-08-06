8429f9043e524170131ecc9c6192096eb3f908e8
Muhammad Hammad <74261526+MuhammadHammad-git@users.noreply.github.com>
2023-08-06 18:23:39 +0500

    Windows Edition
    Added the Capability to be run on Windows instead of Linux!

--- Default_screen_res.py
+++ Default_screen_res.py
@@ -3,8 +3,8 @@ import pygame
 pygame.init()
 # Get the current video display mode
 infoObject = pygame.display.Info()
-screen_height_def = 720#infoObject.current_h
-screen_width_def = 1280#infoObject.current_w
+screen_height_def = infoObject.current_h
+screen_width_def = infoObject.current_w
 
 screen_height_test = 800
 screen_width_test = 800
